276b9e50501f5bced2d4e7e2b249f0bc000790b9
Phoebectw <53845775+Phoebectw@users.noreply.github.com>
2019-08-07 12:35:29 +0800

    Add files via upload

---
+++ L1/Q3.py
@@ -0,0 +1,10 @@
+from math import*
+r=input("what is the radius?")
+A=(pi*(int(r)**2))
+print("the area is ", A, "cm2")
+
+score=float(int("what is your score in exam\n"))
+if (score<60):
+    print("you failed")
+else:
+    print("you passed")
