64e013d821734820645cc762e001f6d7c0e13ee6
Phoebectw <53845775+Phoebectw@users.noreply.github.com>
2019-08-09 09:41:24 +0800

    Add files via upload

---
+++ L1/q4.py
@@ -0,0 +1,14 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================== RESTART: F:\q4 - 複製.py ===========================
+what year ?1995
+Traceback (most recent call last):
+  File "F:\q4 - 複製.py", line 6, in <module>
+    if (year%400 is 0):
+TypeError: not all arguments converted during string formatting
+>>> 
+=========================== RESTART: F:\q4 - 複製.py ===========================
+what year ?1995
+it is not a leap year
+>>> 
