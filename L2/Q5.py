8c0341c8f0f8158b7c7fe0f81a91c6114aaa49cc
Phoebectw <53845775+Phoebectw@users.noreply.github.com>
2019-08-13 14:46:21 +0800

    Add files via upload

--- L2/Q5.py
+++ L2/Q5.py
@@ -29,8 +29,22 @@ print("""
       (_____)                     _______)                  __________)
       (____)                     _______)                  (____)
 ---.__(___)             ---.__________)              ---.__(___)
-
 """) # 1 for rock; 2 for paper; 3 for scissor
+row=0
+while row<3:
+    p_choice=1000
+    
+    while int(p_choice)<1 or int(p_choice)>3:
+        p_choice= (input("please enter 1, 2, or 3"))
+    m_choice= random.randint(1,4)
+    difference= p_choice - m_choice
+    if difference==1 or difference==-2:
+        status=1
+    if difference==-1 or difference==2:
+        status=2
+    if difference==0:
+        status=3
+
 
 # step1: get player's choice, save it in variable p_choice
 
@@ -43,7 +57,7 @@ print("""
 # status is used for the win/lose/draw of the game
 # status = 1 means player wins; status = 2 means minion wins; status = 3 means draw;
 # status = 4 means user gives invalid input, e.g. player inputs -1 or 4
-status = 0 # initialized as 0
+    status = 0 # initialized as 0
 # step 3: given choices from player and minion, decide the game status
 
 
@@ -55,10 +69,21 @@ status = 0 # initialized as 0
 
 
 # step4: display the minion's choice
-if m_choice == 1:
-    print("Minion chooses rock!")
-elif m_choice == 2:
-    print("Minion chooses paper!")
-elif m_choice == 3:
-    print("Minion chooses scissor!")
-
+    if m_choice == 1:
+        print("Minion chooses rock!")
+    elif m_choice == 2:
+        print("Minion chooses paper!")
+    elif m_choice == 3:
+        print("Minion chooses scissor!")
+    if status==1:
+        print("you win")
+        row=row+1
+    if ststus==2:
+        print("you lose")
+        row=0
+    if status==3:
+        print("draw")
+        row=0
+if row ==3:
+    print("you beat the king")
+    
