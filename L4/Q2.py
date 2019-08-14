db8bc3e1f324e951a76a80df48456f2d14354b19
Phoebectw <53845775+Phoebectw@users.noreply.github.com>
2019-08-14 11:54:48 +0800

    Add files via upload

---
+++ L4/Q2.py
@@ -0,0 +1,17 @@
+n=int(input("please input a positive integer"))
+    for x in range(1, n + 1):
+        if n % x == 0 :
+            print("%d divides %d" % (x, n))
+   
+      
+##    factors(40) # print 1 divids 40
+##            #       2 divids 40
+##            #       4 divids 40
+##            #       5 divids 40
+##            #       8 divids 40
+##            #      10 divids 40
+##            #      20 divids 40
+##            #      40 divids 40
+##factors(5)  # print 1 divids 5   
+##            #       5 divids 5
+
